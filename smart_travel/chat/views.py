791825efccbd45325979b3fd2900db60d40d4e95
JSpencerPittman <jspencerpittman@gmail.com>
2025-11-03 12:15:02 -0600

    Add sessions and error handling

--- smart_travel/chat/views.py
+++ smart_travel/chat/views.py
@@ -3,7 +3,6 @@ from typing import Any, Optional
 
 from chat.forms import MessageForm, NewChatForm
 from chat.models import Conversation, User
-from django.core.files.base import ContentFile  # type: ignore
 from django.db.models.query import QuerySet  # type: ignore
 from django.shortcuts import redirect, render, HttpResponseRedirect  # type: ignore
 
@@ -47,7 +46,8 @@ def _find_convos(u: User, chat_id: Optional[int] = None) -> QuerySet:
     return Conversation.objects.filter(**search_query)
 
 
-def _handle_error(message: str) -> HttpResponseRedirect:
+def _handle_error(request, message: str) -> HttpResponseRedirect:
+    request.session["error"] = message
     return redirect("/chat")
 
 
@@ -71,11 +71,16 @@ def select(request):
 
     convos = [(convo.id, convo.title) for convo in _find_convos(curr_user)]
 
+    error = request.session.get("error", None)
+    if "error" in request.session:
+        del request.session["error"]
+
     context = {
         "first_name": curr_user.first_name,
         "last_name": curr_user.last_name,
         "convos": convos,
         "new_chat_form": NewChatForm(),
+        "error": error,
     }
 
     return render(request, "select.html", context)
@@ -83,11 +88,11 @@ def select(request):
 
 def new_chat(request):
     if request.method != "POST":
-        return _handle_error("Invalid new chat request.")
+        return _handle_error(request, "Invalid new chat request.")
 
     form = NewChatForm(request.POST)
     if not form.is_valid():
-        return _handle_error("Invalid new chat request.")
+        return _handle_error(request, "Invalid new chat request.")
 
     curr_user = _get_current_user()
     title = form.cleaned_data["title"]
@@ -103,11 +108,11 @@ def new_chat(request):
 
 def message(request, chat_id: int):
     if request.method != "POST":
-        return _handle_error("Invalid new chat request.")
+        return _handle_error(request, "Invalid new chat request.")
 
     form = MessageForm(request.POST)
     if not form.is_valid():
-        return _handle_error("Invalid message request.")
+        return _handle_error(request, "Invalid message request.")
 
     message = form.cleaned_data["message"]
 
