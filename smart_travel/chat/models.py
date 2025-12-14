c1efff4adf4eaadfce17699079af2649c7c02422
Jeffin Joseph <jjeffin946@gmail.com>
2025-12-14 23:20:12 +0000

    Introduced convoRep Model

--- smart_travel/chat/models.py
+++ smart_travel/chat/models.py
@@ -6,11 +6,18 @@ from django.db import models  # type: ignore
 PROJECT_DIR = Path(__file__).parent.parent
 
 
+class ConvoRepo(models.Model):
+
+    userId = models.CharField(max_length = 50)
+    convoId = models.CharField(max_length = 50)
+
+
+
 class ConversationModel(models.Model):
     MEDIA_DIR = "media/conversations"
 
     title = models.CharField(max_length=50)
-    user = models.ForeignKey(AccountModel, on_delete=models.CASCADE)
+    #user = models.ForeignKey(AccountModel, on_delete=models.CASCADE)
     file_name = models.CharField(max_length=200)
     time_of_last_message = models.DateTimeField()
 
